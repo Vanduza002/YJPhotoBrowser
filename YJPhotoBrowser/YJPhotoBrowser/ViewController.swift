2bb792f9c6c4460d0abfbf73ec8641ea5f733b3a
Vanduza <vanduza@163.com>
2017-01-09 15:45:55 +0800

    create project

---
+++ YJPhotoBrowser/YJPhotoBrowser/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  YJPhotoBrowser
+//
+//  Created by Vanduza on 2017/1/9.
+//  Copyright © 2017年 Vanduza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+}
